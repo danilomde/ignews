28721232501fce9db750e7e359a46fe9df0f971f
Danilo Zeni <danilomde@gmail.com>
2021-07-13 23:33:13 -0300

    init checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,17 +1,27 @@
-import styles from  './styles.module.scss'
+import { signIn, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-  priceId: string
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps){
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
 
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
 
   return (
-      <button type="button" className={styles.subscribeButton}>
-        Subscribe Now
-      </button>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
   );
-  
-  
 }
